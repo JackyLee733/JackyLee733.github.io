4c1ac367270ea7067ef0d8c5e61862bc2849c6ee
JackyLee733 <jackylee733@gmail.com>
2016-09-19 00:15:41 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+<title>乐丰棋牌-山西人自己的游戏网！</title>
+<link rel="shortcut icon" href="../images/favicon.ico" />
+<link href="../style/main.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <div class="bodybox">
+		<?php include("../include/top.php");  ?>
+        <?php include("../include/center.php");  ?>
+        <span class="line_5"></span>
+        <span>
+        	<div class="body_list">
+                <span class="left">
+                    <div class="left_list">
+                        <span class="pop2">
+                            <a class="down_0" href="乐丰棋牌.exe"></a>
+                            <a class="reg_1" href="../user/reg.php" ></a>
+                            <a class="cz_1" href="../pay/ipay.php" ></a>
+                        </span>
+                        <span class="line_5s"></span>
+                        <span class="pop6">
+                            <div class="kfqq1">
+                                <a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=2635163032&site=qq&menu=yes">
+                                <img border="0" src="http://wpa.qq.com/pa?p=2:2635163032:41" alt="点击这里给我发消息" title="点击这里给我发消息"/>
+                                </a>
+                            </div>
+                            <div class="kfqq2">
+                                <a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=2757308588&site=qq&menu=yes">
+                                <img border="0" src="http://wpa.qq.com/pa?p=2:2757308588:41" alt="点击这里给我发消息" title="点击这里给我发消息"/>
+                                </a>
+                            </div>
+                       </span>
+                    </div>
+                </span>
+                <span class="right">
+                    <div class="main_list">
+                   	  <span class="main_tit"><div class="right_title">下载专区</div>
+                   	  </span>
+                    	<span class="main_bg" style=" height:750px;">
+                                   <div class="down_1">
+                                       <div class="dbt_1" onclick="window.open('../client/乐丰棋牌精简版.exe');"></div>
+                                       <div class="dbt_2" onclick="window.open('../client/乐丰棋牌精简版.exe');"></div>
+                                   </div>
+                                   <div class="down_2">
+                                       <div class="dbt_1" onclick="window.open('../client/乐丰棋牌.exe');"></div>
+                                       <div class="dbt_2" onclick="window.open('../client/乐丰棋牌.exe');"></div>
+                                   </div>
+                                   <div class="down_4">
+                                       <div class="dbt_5" onclick="window.open('../download/phone.php');"></div>
+                                   </div>
+                      </span>
+                    	<span class="main_un"></span>
+                    </div>
+                </span>
+            </div>
+        </span>
+        <span class="line_5"></span>
+        <span class="pop9"></span>
+		<?php include("../include/end.php");  ?>
+    </div>
+<script type="text/javascript"  src="../js/zuocefudong_er.js"></script>
+</body>
+</html>
